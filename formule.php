319f6118d179907e50c251ea184c7df2bc805ee1
Antek93 <antonio.savino1993@gmail.com>
2023-02-21 15:32:51 +0100

    aggiunto esercizio piu bonus 1

---
+++ formule.php
@@ -0,0 +1,47 @@
+<?php
+$selected_length = $_GET['range'];
+
+function length_selector($length) {
+    if ($length == "00") {
+        $newLength = 0;
+    } else if ($length == "10") {
+        $newLength = 10;
+    } else if ($length == "20") {
+        $newLength = 20;
+    } else if ($length == "30") {
+        $newLength = 30;
+    } else if ($length == "40") {
+        $newLength = 40;
+    } else if ($length == "50") {
+        $newLength = 50;
+    } else if ($length == "100") {
+        $newLength = 100;
+    }
+
+    return $newLength;
+};
+
+$ultimate_length = length_selector($selected_length);
+
+function generateRandomString($length)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"£$%&/()';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+};
+
+$password = generateRandomString($ultimate_length);
+
+session_start();
+
+$_SESSION["new"] = $password;
+
+if ($password >= 10) {
+    header ('location: ./showpassword.php');
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include __DIR__ . '/formule.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+    <h1>Password generator</h1>
+    <div>
+        <form action="" method="GET">
+            <select label="Quanti caratteri?" name="range" id="">
+                <option selected value="00">0</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+            <button type="submit">
+                Genera password
+            </button>
+        </form>
+    </div>
+</body>
+
+</html>
